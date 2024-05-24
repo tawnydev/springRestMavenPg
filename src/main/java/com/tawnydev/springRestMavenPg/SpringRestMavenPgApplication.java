d61bc4c881053a72d3e0113b8560dff31b730ba0
tawnydev <kevin.matrix@hotmail.fr>
2024-05-24 11:48:01 +0200

    structure with JPA

---
+++ src/main/java/com/tawnydev/springRestMavenPg/SpringRestMavenPgApplication.java
@@ -0,0 +1,13 @@
+package com.tawnydev.springRestMavenPg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestMavenPgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestMavenPgApplication.class, args);
+	}
+
+}
